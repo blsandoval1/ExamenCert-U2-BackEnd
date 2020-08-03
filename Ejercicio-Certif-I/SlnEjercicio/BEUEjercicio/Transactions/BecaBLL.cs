f0cfbc8889f8a9e2a20c3963898647678c41176e
Bryan Sandoval <blsandoval1@espe.edu.ec>
2020-08-03 18:37:08 -0500

    WEB API ANGULAR DE BECAS

---
+++ Ejercicio-Certif-I/SlnEjercicio/BEUEjercicio/Transactions/BecaBLL.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BEUEjercicio.Transactions
+{
+    public class BecaBLL
+    {
+
+        public static void Create(Beca b)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Becas.Add(b);
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static Beca Get(int? id)
+        {
+            Entities db = new Entities();
+            return db.Becas.Find(id);
+        }
+
+        public static void Update(Beca Beca)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        db.Becas.Attach(Beca);
+                        db.Entry(Beca).State = System.Data.Entity.EntityState.Modified;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static void Delete(int? id)
+        {
+            using (Entities db = new Entities())
+            {
+                using (var transaction = db.Database.BeginTransaction())
+                {
+                    try
+                    {
+                        Beca Beca = db.Becas.Find(id);
+                        db.Entry(Beca).State = System.Data.Entity.EntityState.Deleted;
+                        db.SaveChanges();
+                        transaction.Commit();
+                    }
+                    catch (Exception ex)
+                    {
+                        transaction.Rollback();
+                        throw ex;
+                    }
+                }
+            }
+        }
+
+        public static List<Beca> List()
+        {
+            Entities db = new Entities();
+            return db.Becas.ToList();
+        }
+
+
+    }
+}
---
+++ Ejercicio-Certif-I/SlnEjercicio/WebApiEscolastico/Controllers/BecaController.cs
@@ -0,0 +1,85 @@
+﻿using BEUEjercicio;
+using BEUEjercicio.Transactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using System.Web.Http.Description;
+using System.Web.Mvc;
+
+namespace WebApiEscolastico.Controllers
+{
+    [EnableCors(origins: "http://localhost:4200", headers: "*", methods: "*")]
+    public class BecaController : ApiController
+    {
+        public IHttpActionResult Post(Beca beca)
+        {
+            try
+            {
+                BecaBLL.Create(beca);
+                return Content(HttpStatusCode.Created, "Beca creado correctamente");
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+                       
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                List<Beca> todos = BecaBLL.List();
+                return Content(HttpStatusCode.OK, todos);
+            }
+            catch (Exception ex) {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Put(Beca beca)
+        {
+            try
+            {
+                BecaBLL.Update(beca);
+                return Content(HttpStatusCode.OK, "Beca actualizado correctamente");
+
+            }
+            catch (Exception ex) {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                Beca result = BecaBLL.Get(id);
+                if (result == null) {
+                    return NotFound();
+                }
+                return Content(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                BecaBLL.Delete(id);
+                return Ok("Beca eliminado correctamente");
+            }
+            catch (Exception ex)
+            {
+                return Content(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+    }
+}
